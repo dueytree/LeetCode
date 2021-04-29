dc7dee68febcc33bcc8e254c3741a5aa1d9e3908
dueytree <dueytree.00@gmail.com>
2021-04-30 07:31:11 +0900

    revice

--- Reverse_String.py
+++ Reverse_String.py
@@ -1,12 +1,14 @@
 # Reverse String
-]# https://leetcode.com/problems/reverse-string/submissions/
+# https://leetcode.com/problems/reverse-string/submissions/
 
 # 단순 문자열을 뒤집는 문제이다. reverse 함수로 간단히 뒤집어 주면 된다.
 # 두번째 풀이는 reverse 함수를 이용하지 않은 풀이다. 왼쪽, 오른쪽 포인트 하나씩으로 while문을 돌리고,
 # 왼쪽 오른쪽 인덱스 값으로 서로를 바꿔주는 방법을 이용한다.
 
-def reverseString(s) -> None:
+def reverseString(s):
     s.reverse()
+    return s
+
 
 def test_solution():
     assert reverseString(["h", "e", "l", "l", "o"]) == ["o", "l", "l", "e", "h"]
